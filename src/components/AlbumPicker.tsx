f98d4ce434cb205a249594d97022f170be1332c6
NumanBashir <sportybashir@gmail.com>
2024-01-29 16:53:36 +0100

    Added date next to album name

--- src/components/AlbumPicker.tsx
+++ src/components/AlbumPicker.tsx
@@ -1,7 +1,9 @@
 import { FormEvent, useState } from "react";
+import "../App.css";
 
 function AlbumPicker() {
   const [albums, setAlbums] = useState<string[]>([]);
+  const [releaseDates, setReleaseDates] = useState<string[]>([]);
   async function handleSubmit(e: FormEvent) {
     e.preventDefault();
     const target = e.target as typeof e.target & {
@@ -11,22 +13,27 @@ function AlbumPicker() {
     const url = `https://musicbrainz.org/ws/2/release?fmt=json&query=artist:${artist}`;
     const response = await fetch(url);
     const mbResult = (await response.json()) as {
-      releases: { title: string }[];
+      releases: { title: string; date: string }[];
     };
     const { releases } = mbResult;
     setAlbums(releases.map(({ title }) => title));
+    setReleaseDates(releases.map(({ date }) => date));
+    console.log(mbResult);
   }
+
   return (
     <form onSubmit={handleSubmit}>
       <label>
         Artist name:
         <input name="artist" />
       </label>
-      <button type="submit">Search</button>
+      <button className="my-search-button" type="submit">
+        Search
+      </button>
       <p>Albums:</p>
       <ol>
-        {albums.map((album) => (
-          <li>{album}</li>
+        {albums.map((album, index) => (
+          <li key={index}>{`${album} - ${releaseDates[index]}`}</li>
         ))}
       </ol>
     </form>
