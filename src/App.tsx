1ddd9092c61b23ff970af02ce7870bf24218ff6b
NumanBashir <sportybashir@gmail.com>
2024-01-29 13:35:03 +0100

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import AlbumPicker from "../src/components/AlbumPicker";
+
+function App() {
+  return (
+    <>
+      <div>
+        <AlbumPicker />
+      </div>
+    </>
+  );
+}
+
+export default App;
